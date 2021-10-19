49038d2468659aecfe3696eae756b7fff641128f
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-19 10:04:58 +0530

    Adding Welcome Message

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Day_13_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics");
+
+        }
+    }
+}
