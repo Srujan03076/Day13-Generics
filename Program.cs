996f9633b1f15af584e3a846523a913025e87c53
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-19 10:15:09 +0530

    Adding Welcome Message

--- Program.cs
+++ Program.cs
@@ -11,9 +11,8 @@ namespace Day_13_Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find max Number");
-            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
-            Console.WriteLine(output);
+            Console.WriteLine("Welocme to Generics");
+            
         }
     }
 }
